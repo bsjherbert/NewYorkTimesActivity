ac91f6ae919f9f04808f1e7aad964ecbba74df85
GabrielTMangum <gabrieltmangum@gmail.com>
2019-10-30 13:50:12 -0400

    added a few variables so we can bring up the object containing all of the NYT articles

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// Change the id to match HTML
+var search = $("#").val();
+
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=6w4Wjqf4AIIU9K23h16tGQ2WiS6GzlqZ"
