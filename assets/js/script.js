9bc842367091c4638221b38fc157b5844a03ccc3
GabrielTMangum <gabrieltmangum@gmail.com>
2019-10-30 14:20:11 -0400

    added some ids to index.html to hook into my js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Change the id to match HTML
-var search = $("#").val();
-var startYear = $("#").val();
-var endYear = $("#").val();
-var numberOfRecords = $("#").val();
+var search = $("#searchInput").val();
+var startYear = $("#startYear").val();
+var endYear = $("#endYear").val();
+var numberOfRecords = $("#searchLimits").val();
 
 var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + startYear + "0101&end_date=" + endYear + "0101&q=" + search + "&sort=newest&api-key=6w4Wjqf4AIIU9K23h16tGQ2WiS6GzlqZ"
