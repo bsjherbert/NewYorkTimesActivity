3e9453c17d67fcd7cdbd9da74816e7f762329e3c
GabrielTMangum <gabrieltmangum@gmail.com>
2019-10-30 14:03:30 -0400

    added variables to reflect search parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 // Change the id to match HTML
 var search = $("#").val();
+var startYear = $("#").val();
+var endYear = $("#").val();
+var numberOfRecords = $("#").val();
 
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=6w4Wjqf4AIIU9K23h16tGQ2WiS6GzlqZ"
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + startYear + "0101&end_date=" + endYear + "0101&q=" + search + "&sort=newest&api-key=6w4Wjqf4AIIU9K23h16tGQ2WiS6GzlqZ"
