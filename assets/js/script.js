9735474cb34713bd7cab9cbc382199d138a685ee
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-10-31 10:30:51 -0400

    added eventprevent

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,8 @@ var endYear = $("#endYear").val();
 var numberOfRecords = $("#searchLimits").val();
 var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + startYear + "0101&end_date=" + endYear + "0101&q=" + search + "&sort=newest&api-key=6w4Wjqf4AIIU9K23h16tGQ2WiS6GzlqZ"
 
-$("#testbutton").on("click", function () {
+$("#testbutton").on("click", function (event) {
+    event.preventDefault();
     $.ajax({
         url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=19890101&end_date=20190101&q=fires&sort=newest&api-key=6w4Wjqf4AIIU9K23h16tGQ2WiS6GzlqZ",
         method: "GET"
